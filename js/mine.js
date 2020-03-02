2c1a0051665bb3aefd242a014718b88193a0aa63
abdomekki <abdo.mekki93@gmail.com>
2020-03-02 12:07:55 +0200

    v

---
+++ js/mine.js
@@ -0,0 +1,25 @@
+//var circle1 =document.querySelector(".circle")
+//
+//
+//circle1.addEventListener("click", function(){
+//   $(document).keydown(function(e){
+//    if(e.keyCode == 39)
+//        {
+//            $(".rocket-container").css("left","+=50px");
+//        }
+//    
+//})
+//    
+    
+    
+    
+    
+
+$( function() {
+    $( "#draggable" ).draggable();
+  } );
+
+
+$( function() {
+    $( "#draggablee" ).draggable();
+  } );
